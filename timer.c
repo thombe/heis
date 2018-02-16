c54aa40c27dd1acb5c4fe12650e9cf99188fcb7b
thombe <thomasberntsen97@gmail.com>
2018-02-16 11:25:57 +0100

    adding timer module

---
+++ timer.c
@@ -0,0 +1,35 @@
+/*
+Timer module for TTK4235 elevator
+Module provides simple timer functions for elevator.
+Only works for integer amount of seconds
+2018 thombe
+*/
+
+#include "timer.h"
+#include "sys/time.h"
+
+
+static int t = 0;
+static int duration;
+
+int get_wall_time(void)
+{
+    struct timeval time;
+    gettimeofday(&time , NULL);
+    return (int)time.tv_sec + (int)time.tv_usec * 0.000001;
+}
+
+void start_timer(int dur)
+{
+    t = get_wall_time();
+    duration = dur;
+}
+
+void stop_timer(void)
+{
+    t = 0;
+}
+
+int duration_passed(void) {
+    return (get_wall_time() - t) >= duration;
+}
---
+++ timer.h
@@ -0,0 +1,29 @@
+/*
+Timer module for TTK4235 elevator
+Module provides simple timer functions for elevator.
+Only works for integer amount of seconds
+2018 thombe
+*/
+
+
+#ifndef timer_H
+#define timer_H
+
+// Function converts timeval struct to double
+// From "Intro-to-C-Linux" TTK4235
+
+int get_wall_time(void);
+
+// Function starts timer and sets duration to int dur.
+
+void start_timer(int dur);
+
+// Function stops/resets timer. Not really that helpful for this project.
+
+void stop_timer(void);
+
+// Function checks whether current time - start time > duration
+
+int duration_passed(void);
+
+#endif
