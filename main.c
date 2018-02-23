cefe43c9d9be9b6dea9653f29d14980b5d689d1b
thombe <thomasberntsen97@gmail.com>
2018-02-23 11:15:44 +0100

    init

--- main.c
+++ main.c
@@ -16,16 +16,7 @@ int main() {
 
 
     while (1) {
-        //initialize();
-        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
-            elev_set_motor_direction(DIRN_DOWN);
-        } else if (elev_get_floor_sensor_signal() == 0) {
-            elev_set_motor_direction(DIRN_UP);
-        }
-        if (elev_get_stop_signal()) {
-            elev_set_motor_direction(DIRN_STOP);
-            break;
-        }
+        initialize();
     }
 
 
