be4bcf05d29037562d5c866ddbf1edf1549f9daa
thombe <thomasberntsen97@gmail.com>
2018-03-02 10:49:04 +0100

    t

--- state.c
+++ state.c
@@ -121,9 +121,9 @@ void run_state_machine()
                 printf("current direction is %d\n", get_DIR());
                 change_state(ATFLOOR);
             } else if (cur_floor == cur_ord) {
-                printf("Current floor is %d and current direction is %d\n", last_floor , get_DIR());
+                printf("Current floor is %d and current direction is %d\n", cur_floor , get_DIR());
                 change_state(ATFLOOR);
-                printf("Current floor is %d and current direction is %d\n", last_floor , get_DIR());
+                printf("Current floor is %d and current direction is %d\n", cur_floor , get_DIR());
             }
             if (cur_floor!= -1) {
                 elev_set_floor_indicator(cur_floor);
