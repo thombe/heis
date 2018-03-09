0feb9e2d15f4141633177b2a49b0bd7c738b84aa
thombe <thombe@stud.ntnu.no>
2018-03-09 10:19:23 +0100

    main clean

--- main.c
+++ main.c
@@ -13,8 +13,6 @@ int main() {
         return 1;
     }
 
-    printf("Press STOP button to stop elevator and exit program.\n");
-
 
     while (1) {
         run_state_machine();
@@ -27,23 +25,3 @@ int main() {
 
     return 0;
 }
-
-/*
-
-        elev_set_motor_direction(DIRN_UP);
-        while (1) {
-        // Change direction when we reach top/bottom floor
-        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
-            elev_set_motor_direction(DIRN_DOWN);
-        } else if (elev_get_floor_sensor_signal() == 0) {
-            elev_set_motor_direction(DIRN_UP);
-        }
-
-        // Stop elevator and exit program if the stop button is pressed
-        if (elev_get_stop_signal()) {
-            elev_set_motor_direction(DIRN_STOP);
-            break;
-        }
-	add_order();
-    }
-*/
